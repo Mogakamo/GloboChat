c63e3706c464edcf38ed09c3939aa958255a65b5
Mogakamo <com02519@uoeld.ac.ke>
2021-11-23 11:08:54 +0300

    solved the build errors and added the files for the GloboChat Application

--- app/src/main/java/com/example/globochat/SettingsFragment.kt
+++ app/src/main/java/com/example/globochat/SettingsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
+import com.example.globochat.R
 
 
 class SettingsFragment : Fragment() {
@@ -13,7 +14,8 @@ class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View ? {
+
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
         view.setOnClickListener {
